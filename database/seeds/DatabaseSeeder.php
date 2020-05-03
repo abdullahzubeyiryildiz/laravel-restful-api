90e2c807e7f4d3dc20c9784db7d0ea67fc43d8df
Abdullah Zübeyir Yıldız <zubeyir_55@hotmail.com>
2020-05-04 00:59:19 +0300

    Seed Oluşturuldu.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(ProductTableSeeder::class);
     }
 }
